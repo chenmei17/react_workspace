efb17afd67d90ea047195947b4f384526fd93331
chenmei17 <cocoa2675@gmail.com>
2026-01-27 15:51:51 +0900

    비동기 로그인을 진행해보자

--- viteapp/src/api/auth.js
+++ viteapp/src/api/auth.js
@@ -2,11 +2,49 @@ const BASE_URL = "http://localhost:9993/api/auth"; // 서버주소
 
 /*
     로그인 요청
+    payload 는 js 객체 리터럴
 */
 
-function loginRequest(){
+export function loginRequestWithJson(payload){
     console.log("비동기로그인 요청예정..");
-    fetch(BASE_URL,{
+    fetch(`${BASE_URL}/login`,{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        credentials:"include", // 비동기식 쿠키전달을 위해 쿠키포함하여 전송함을 선언 
+        body:JSON.stringify(payload)
+    })
+    .then(res=>{
+        if(!res.ok) throw new Error("로그인에러");
+        return res.json(); // promise 를 반환하면서 동시에 resolve(json문자열을 parsing한 객체 )를 호출 
+    })
+    .then(data => {
+        console.log("로그인 결과...",data)
+    })
+    .catch(err=>console.log(err));
+}
 
-    });
+// JSON 문자열 전송이 아닌 form 전송 방식. 
+
+export function loginRequest(payload){
+
+    // form 방식의 파라미터를 전송할 수 있는 객체 
+    const params = new URLSearchParams(); 
+    params.append("homepageId", payload.homepageId);
+    params.append("password", payload.password);
+
+    fetch(`${BASE_URL}/login`,{
+        method:"POST",
+        headers:{"Content-Type":"application/x-www-form-urlencoded"},
+        credentials:"include",
+        body:params.toString()
+    })
+    .then(res=>{
+        if(!res.ok) throw new Error("로그인에러");
+        return res.json(); // promise 를 반환하면서 동시에 resolve(json문자열을 parsing한 객체 )를 호출 
+    })
+    .then(data => {
+        console.log("로그인 결과...",data)
+    })
+    .catch(err=>console.log(err));
+
 }
--- viteapp/src/pages/Login.jsx
+++ viteapp/src/pages/Login.jsx
@@ -1,20 +1,34 @@
 import uiStyles from "../components/ui/input.module.css";
 import {loginRequest} from "../api/auth.js";
+import { useRef } from "react";
 
 
 export default function Login() {
+   // Ref 값 가져오기 
+   // useRef 로 선언하면 해당대상은 객체!! 
+   // current 를 이용하여 데이터를 넣을 수 있다. 
+   const homepageIdRef = useRef(null);
+   const passwordRef = useRef(null);
    
    // 로그인 요청 메서드 정의 
    const login = ()=>{
-      fetch();
+      
+      // React 에서 사용자가 입력한 homepageId , password 
+      const payload = {
+         homepageId: homepageIdRef.current.value,
+         password: passwordRef.current.value
+      };
+
+      loginRequest(payload);
    }
+
    return (
       <div style={{maxWidth:"400px",margin:"0 auto"}}>
          <div>
             <h2>로그인</h2>
             <form style={{display:"flex",flexDirection:"column",gap:10}}>
-               <input type="text" placeholder="아이디 입력" className={uiStyles.inputStyle} />
-               <input type="password" placeholder="비밀번호입력" className={uiStyles.inputStyle} />
+               <input type="text"      placeholder="아이디 입력" className={uiStyles.inputStyle} ref={homepageIdRef} />
+               <input type="password"  placeholder="비밀번호입력" className={uiStyles.inputStyle} ref={passwordRef} />
                <button type="button" className={uiStyles.primaryBtn} onClick={login}>로그인</button>
             </form>
          </div>
